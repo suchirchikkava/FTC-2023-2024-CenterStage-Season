d29069b7944fac1675af9b0242638d788babdbc6
VishalManikanden <vishy.mani@gmail.com>
2023-11-01 20:18:33 -0400

    Auto code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VishalAutoFarLeftNoBackdrop.java
@@ -0,0 +1,70 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name="VishalAutoFarLeftNoBackdrop")
+public class VishalAutoFarLeftNoBackdrop {
+    private static DcMotor frontLeft;
+    private static DcMotor backLeft;
+    private static DcMotor frontRight;
+    private static DcMotor backRight;
+
+    public void runWheels() {
+        while (frontRight.getCurrentPosition() != frontRight.getTargetPosition()) {
+            frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        }
+    }
+
+    public void reset() {
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+    public void moveForward(int distance) {
+        reset();
+        frontLeft.setTargetPosition(distance);
+        frontRight.setTargetPosition(distance);
+        backLeft.setTargetPosition(distance);
+        backRight.setTargetPosition(distance);
+        runWheels();
+    }
+
+    public void moveBackward(int distance) {
+        reset();
+        frontLeft.setTargetPosition(-distance);
+        frontRight.setTargetPosition(-distance);
+        frontLeft.setTargetPosition(-distance);
+        frontRight.setTargetPosition(-distance);
+        runWheels();
+    }
+
+    public void strafeLeft(int distance) {
+        reset();
+        frontLeft.setPower(-distance);
+        backLeft.setPower(distance);
+        frontRight.setPower(distance);
+        backRight.setPower(-distance);
+        runWheels();
+    }
+
+    public void strafeRight(int distance) {
+        reset();
+        frontLeft.setPower(distance);
+        backLeft.setPower(-distance);
+        frontRight.setPower(-distance);
+        backRight.setPower(distance);
+        runWheels();
+    }
+}
+
+    // Scan for placement of custom team element using machine learning to see which of the three
+    // locations has the highest percentage of certainty
+
+    // Place purple tag on tape
+}
